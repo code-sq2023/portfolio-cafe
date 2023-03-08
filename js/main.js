d64d8e41c1ec71d3130eea69bbc02d0b2f13804d
code-sq2023 <info@code-sq.com>
2023-03-08 21:21:35 +0900

    (add)header,mainvisual,about完了

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  /*メインビジュアルのスライダー設定*/
+
+  $(".mainvisual-list").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    slidesToShow:1,
+    centerMode: true,
+    centerPadding: '10%',
+    speed: 700,
+    arrows: false,
+    draggable: false
+  })
+});
